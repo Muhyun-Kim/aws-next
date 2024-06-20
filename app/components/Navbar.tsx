855d066039801a23371bb951cf984757cffc7dff
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2024-06-21 00:20:06 +0900

    start project

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,27 @@
-import { AppBar } from "@mui/material";
+'use client';
+
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
 
 export default function Navbar() {
-  return <AppBar position="static"></AppBar>;
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        ></IconButton>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          MyApp
+        </Typography>
+        <Button color="inherit" component="a" href="/sign-in">
+          Sign-In
+        </Button>
+        <Button color="inherit" component="a" href="/sign-up">
+          Sign-Up
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
